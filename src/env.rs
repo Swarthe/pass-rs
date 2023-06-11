dc4bc40fa1f6036645d75242e4858ff26e64528d
Emil Overbeck <emil.a.overbeck@gmail.com>
2023-06-11 17:36:17 +0200

    Add copyright year to version text

--- src/env.rs
+++ src/env.rs
@@ -214,7 +214,7 @@ impl Display for Version {
         write!(
             f, concat!(
 env!("CARGO_PKG_NAME"), " ", env!("CARGO_PKG_VERSION"), "
-Copyright (C) ", env!("CARGO_PKG_AUTHORS"), ".
+Copyright (C) 2023 ", env!("CARGO_PKG_AUTHORS"), ".
 License ", env!("CARGO_PKG_LICENSE"), " <{}>.
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law."
