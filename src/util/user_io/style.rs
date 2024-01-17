4c3cfca8cb7d67904a4db67d0c02e8fd2e0af4e2
Emil Overbeck <emil.a.overbeck@gmail.com>
2024-01-17 19:08:20 +0100

    Improve text styling to detect terminal support

--- src/util/user_io/style.rs
+++ src/util/user_io/style.rs
@@ -1,86 +1,74 @@
-use once_cell::sync::Lazy;
-
 use owo_colors::AnsiColors;
 
-use std::{env, fmt};
+use supports_color::Stream::{self, Stdout, Stderr};
+
+use std::fmt;
 
 use std::fmt::Display;
 
-/// Support the [`NO_COLOR`](https://no-color.org/) convention.
-static NO_STYLE: Lazy<bool> = Lazy::new(||
-    env::var_os("NO_COLOR").is_some()
-);
-
-/// returns from the calling function with the passed parameter in a plain
-/// (unstyled) `StyledMsg` object, if `NO_STYLE` is true.
-macro_rules! return_if_no_style {
-    ($msg:expr) => {{
-        if *NO_STYLE {
-            return StyledMsg {
-                msg: $msg,
-                style: owo_colors::Style::new()
-            }
-        }
-    }}
-}
-
-/// XXX: does nothing if `NO_COLOR` is set
+/// For a text object that can be styled with ANSI codes.
 ///
-/// the implementation for `str` performs no heap allocations, and doesn't copy
-/// the styled string. it is therefore usable for sensitive strings.
+/// The functions only style the object if the terminal and relevant stream
+/// support it, otherwise they return an unstyled `StyledMsg`. The
+/// [`NO_COLOR`](https://no-color.org/) convention is supported.
+///
+/// The implementation for `str` performs no heap allocations, and doesn't copy
+/// the styled string. It is therefore usable for sensitive strings.
 pub trait Style: Display {
     fn as_error(&self) -> StyledMsg<&Self> {
-        return_if_no_style!(self);
-        StyledMsg::new(self, AnsiColors::BrightRed)
+        maybe_colour(self, Stderr, AnsiColors::BrightRed)
     }
 
     fn as_warning(&self) -> StyledMsg<&Self> {
-        return_if_no_style!(self);
-        StyledMsg::new(self, AnsiColors::Yellow)
+        maybe_colour(self, Stderr, AnsiColors::Yellow)
     }
 
     fn as_notice(&self) -> StyledMsg<&Self> {
-        return_if_no_style!(self);
-        StyledMsg::new(self, AnsiColors::Blue)
+        maybe_colour(self, Stderr, AnsiColors::Blue)
     }
 
     fn as_prompt(&self) -> StyledMsg<&Self> {
-        return_if_no_style!(self);
-        StyledMsg::new(self, AnsiColors::Cyan)
+        maybe_colour(self, Stderr, AnsiColors::Cyan)
     }
 
     fn as_title(&self) -> StyledMsg<&Self> {
-        return_if_no_style!(self);
-        StyledMsg::new(self, AnsiColors::Green)
+        maybe_colour(self, Stdout, AnsiColors::Green)
     }
 
     fn as_heading(&self) -> StyledMsg<&Self> {
-        return_if_no_style!(self);
-        StyledMsg::new(self, AnsiColors::BrightMagenta)
+        maybe_colour(self, Stdout, AnsiColors::BrightMagenta)
     }
 
     fn as_name(&self) -> StyledMsg<&Self> {
-        return_if_no_style!(self);
-
-        StyledMsg {
-            msg: self,
-            style: owo_colors::Style::new().bold()
+        if supports_color::on_cached(Stdout).is_some() {
+            StyledMsg::with_style(self, owo_colors::Style::new().bold())
+        } else {
+            StyledMsg::new(self)
         }
     }
 }
 
 impl Style for str {}
 
-// we cannot use `owo_colors::Styled` because it can only be created as a double
-// reference (e.g. `Styled<&&str>`), which upsets the borrow checker when we try
-// to return it.
+// we cannot use `owo_colors::Styled` or `owo_colors::SupportsColorsDisplay`
+// because it can only be created as a double reference (e.g. `Styled<&&str>`),
+// which upsets the borrow checker when we try to return it.
 pub struct StyledMsg<M> {
     msg: M,
     style: owo_colors::Style
 }
 
 impl<M: Display> StyledMsg<M> {
-    fn new(msg: M, colour: AnsiColors) -> Self {
+    /// Returns a `StyledMsg` without an associated style.
+    fn new(msg: M) -> Self {
+        StyledMsg { msg, style: owo_colors::Style::new() }
+    }
+
+    fn with_style(msg: M, style: owo_colors::Style) -> Self {
+        Self { msg, style }
+    }
+
+    fn with_colour(msg: M, colour: AnsiColors) -> Self {
         let style = owo_colors::Style::new()
             .bold().color(colour);
 
@@ -93,5 +81,18 @@ impl<M: Display> Display for StyledMsg<M> {
         use owo_colors::OwoColorize;
 
         write!(f, "{}", self.msg.style(self.style))
+    }
+}
+
+/// If styling is enabled on `strm`, returns `msg` coloured with `col`.
+/// Otherwise, returns an unstyled `StyledMsg`.
+fn maybe_colour<M>(msg: &M, strm: Stream, col: AnsiColors) -> StyledMsg<&M>
+    where
+        M: Display + ?Sized
+{
+    if supports_color::on_cached(strm).is_some() {
+        StyledMsg::with_colour(msg, col)
+    } else {
+        StyledMsg::new(msg)
     }
 }
